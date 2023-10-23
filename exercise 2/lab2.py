cffa785d08e8bbc76af06ac92ad605fc5f9927ff
inlaidw <PRAULGABRIELB09.22@ichat.sp.edu.sg>
2023-10-23 10:47:39 +0800

    excersise 2 n 3

---
+++ exercise 2/lab2.py
@@ -0,0 +1,16 @@
+def display_main_menu():
+ print("display_main_menu")
+
+ def  calc_average():
+     print("calc_average")
+
+def get_user_input():
+    print("list of floats")
+    return
+def find_min_max():
+    print("find_min_max")
+    return
+def sort_temperature():
+    print("sort_temperature")
+def calc_median_tempreture():
+    print("calc_median_tempreture")
